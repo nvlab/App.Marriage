beb0428d4ed274bcebad916f843f06d4977d6235
nvlab <najdat.alaliwi@gmail.com>
2017-04-30 18:57:45 +0300

    الاصدار الاول

---
+++ App.Marriage/App.Marriage/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(App.Marriage.Startup))]
+namespace App.Marriage
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
