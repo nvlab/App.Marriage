4c83eeebcf00aab2ec7e4c57d7072b076903e229
monzer al-kiami <monzer1111@hotmail.com>
2017-05-06 10:29:11 +0300

    Comment 01

---
+++ App.Marriage/App.Marriage/Controllers/FindPersonController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace App.Marriage.Controllers
+{
+    public class FindPersonController : Controller
+    {
+        // GET: FindPerson
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ App.Marriage/App.Marriage/Entity/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace App.Marriage.Entity
+{
+    public class Person
+    {
+        private int _Id;
+
+        public int Id
+        {
+            get { return _Id; }
+            set { _Id = value; }
+        }
+
+    }
+}
