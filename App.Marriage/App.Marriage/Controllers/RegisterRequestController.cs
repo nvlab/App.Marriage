01c96805cb5f2875b0529b64ec62ac41ebb0b9c5
nvlab <najdat.alaliwi@gmail.com>
2017-05-06 15:08:34 +0300

    اضافة شاشة ادارة الطلبات

---
+++ App.Marriage/App.Marriage/Controllers/RegisterRequestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace App.Marriage.Controllers
+{
+    public class RegisterRequestController : Controller
+    {
+        // GET: RegisterRequest
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ App.Marriage/App.Marriage/Models/RegisterRequesMV/RegisterRequestViewModels.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace App.Marriage.Models.RegisterRequesMV
+{
+    public class RegisterRequestViewModels
+    {
+    }
+}
